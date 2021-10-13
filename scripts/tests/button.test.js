0edfa193867b3e2afe6b27cccf7558d04cf1be41
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-10-13 14:01:20 +0000

    completed

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -4,14 +4,23 @@
 
  const buttonClick = require("../button");
 
+ // This function reads the html page
  beforeAll(() => {
-     document.body.innerHTML = "<p id='par'></p>";
+     let fs = require("fs");
+     let fileContents = fs.readFileSync("index.html", "utf-8");
+     document.open();
+     document.write(fileContents);
+     document.close();
  });
  
+ // This carries out the mock testing
  describe("DOM tests", () => {
      test("Expects content to change", () => {
          buttonClick();
          expect(document.getElementById("par")
              .innerHTML).toEqual("You Clicked");
+     });
+     test("h1 should exist", () => {
+         expect(document.getElementsByTagName("h1").length).toBe(1);
      });
  });
